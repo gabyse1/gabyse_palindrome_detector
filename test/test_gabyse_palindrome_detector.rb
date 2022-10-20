365bf44a96c084a96e5ea86c3be3024f7e7a7757
Gaby <gaby.es.sd@gmail.com>
2022-10-20 17:17:00 -0500

    Remove default test suite.

--- test/test_gabyse_palindrome_detector.rb
+++ test/test_gabyse_palindrome_detector.rb
@@ -6,8 +6,4 @@ class TestGabysePalindromeDetector < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::GabysePalindromeDetector::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
