a66c51ba8bbbbadb5cec7e314ab3868cc8c0ae01
Gaby <gaby.es.sd@gmail.com>
2022-10-20 17:29:49 -0500

    Create the gem methods.

--- lib/gabyse_palindrome_detector.rb
+++ lib/gabyse_palindrome_detector.rb
@@ -3,6 +3,27 @@
 require_relative "gabyse_palindrome_detector/version"
 
 module GabysePalindromeDetector
-  class Error < StandardError; end
-  # Your code goes here...
+  def palindrome?
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
+  end
+
+  private
+
+  def processed_content
+    to_s.scan(/[\w\d]+/).join.downcase
+  end
+end
+
+# Adding palindrome method to the String class
+class String
+  include GabysePalindromeDetector
+end
+
+# Adding palindrome method to the Integer class
+class Integer
+  include GabysePalindromeDetector
 end
