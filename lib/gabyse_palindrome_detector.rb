47c25ec40d915a5ff1ef57e8fb78e0c079119d82
Gaby <gaby.es.sd@gmail.com>
2022-10-20 17:03:49 -0500

    Initialize repository

---
+++ lib/gabyse_palindrome_detector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gabyse_palindrome_detector/version"
+
+module GabysePalindromeDetector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gabyse_palindrome_detector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GabysePalindromeDetector
+  VERSION = "0.1.0"
+end
